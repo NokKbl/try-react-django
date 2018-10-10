d970c8ae9af490b3e1149ac21b35686fe3497abe
NokKbl <asdfghjklnokky@gmail.com>
2018-10-10 20:12:33 +0700

    change wn

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
